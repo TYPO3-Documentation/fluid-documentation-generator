465cc8281791ec55dc6cf9dd2a3efb1509a84bfe
Simon Praetorius <simon@praetorius.me>
2024-07-01 19:58:20 +0200

    [TASK] Implement help command

--- src/ConsoleRunner.php
+++ src/ConsoleRunner.php
@@ -54,7 +54,30 @@ final class ConsoleRunner
 
     public function handleHelpCommand(): string
     {
-        return 'TODO help text';
+        return <<< HELP
+----------------------------------------------------------------------------------------------
+                      Fluid Documentation Generator: Help text
+----------------------------------------------------------------------------------------------
+
+Supported commands:
+
+    bin/fluidDocumentation help        # Show this help screen
+    bin/fluidDocumentation generate    # Generates documentation in RST format based on
+                                       # supplied configuration files and available ViewHelpers
+                                       # in the project and writes the result into a new folder
+                                       # named "fluidDocumentationOutput"
+
+Example usage:
+
+    bin/fluidDocumentation generate config/my_viewhelpers.json config/other_viewhelpers.json
+
+The supplied config files must respect the provided JSON Schema located in
+src/Config.schema.json.
+
+Note that the technical implementation uses composer's autoloader to find ViewHelper classes
+in the current project, which means that this command only works in an installed composer
+project.
+HELP;
     }
 
     /**
